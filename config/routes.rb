bcd06292460120dbc25820cdd9ee40696b93a7a5
Jason Hobbs <grimmstede@feemon.com>
2015-09-20 20:31:07 -0500

    sign in work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
   root 'pages#home'
+  get 'register' => 'users#new'
+  get 'sign_in' => 'sessions#new'
+  get 'sign_out' => 'sessions#destroy'
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
 end
