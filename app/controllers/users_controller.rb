0448fc54e0afb6702d25d8446d5271a2796bce02
Jason Hobbs <grimmstede@feemon.com>
2015-09-30 15:14:41 -0500

    admin check back in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
 
   def new
     if current_user
-      redirect_to @user, notice: "already signed in!"
+      unless current_user_admin?
+        redirect_to root_path, notice: "already signed in!"
+      end
     else
   	  @user=User.new
     end
@@ -15,8 +17,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-  	  session[:user_id] = @user.id
-    	redirect_to @user, notice: "Thanks for signing up!"
+      unless current_user_admin?
+  	     session[:user_id] = @user.id
+    	  redirect_to root_path, notice: "Thanks for signing up!"
+      else
+        redirect_to root_path
+      end
     else
     	render :new
     end
