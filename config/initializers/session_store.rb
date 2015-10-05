4f204b416ca61cfd2e6c13c94b6cf25e0efc7565
Jason Hobbs <grimmstede@feemon.com>
2015-10-05 13:11:01 -0500

    httponly on session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_starter_bootstrap_session'
+Rails.application.config.session_store :cookie_store, key: '_starter_bootstrap_session',
+                                                      expire_after: 30.days,
+                                                      :httponly => true
