e0150946e528e78c14e94737d15dd522de9e99bc
Jason Hobbs <grimmstede@feemon.com>
2015-10-15 14:28:27 -0500

    added session tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
+require 'database_cleaner'
 
-RSpec.describe SessionsController, type: :controller do
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
 
+user = User.create!(user_attributes)
+describe SessionsController do
+  context "when signed in" do
+    before do
+      session[:user_id] = user.id
+    end
+    it "logs user out" do
+      delete :destroy, id: user
+      expect(response).to redirect_to(root_path)
+    end
+  end
 end
